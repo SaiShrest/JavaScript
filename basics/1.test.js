852abdb6ade500790e3594079101ef837345648e
SaiShrest <120594046+SaiShrest@users.noreply.github.com>
2024-11-19 14:19:16 +0000

    datatypes

---
+++ basics/1.test.js
@@ -0,0 +1,8 @@
+console.log("manu")
+const name = "ramu"
+let age = 98
+place = "jaipur"
+state = "goa"
+let kamusari
+phone_No = 1991991999
+console.table([name,age,place,state,phone_No,kamusari])
---
+++ basics/2.Datatypes.js
@@ -0,0 +1,18 @@
+console.log(3 + 3);
+let name = "shrest"
+let age = 18
+let istrue = false
+// Primitive Data Types
+// number =>2^53
+// bigint
+// string =>""
+// boolean => true/false
+// NULL => empty value
+// undefined => value is not defined
+// symbol => unique
+
+
+// Object
+
+console.log(typeof undefined);
+console.log(typeof null);
